9948fc75e20bc37f1bd421ed188bcff9eba93dc8
Eric H Martanto <1513209601@qq.com>
2020-01-09 01:48:04 +0700

    add heroku requirements for django projects

--- take_home_project/settings.py
+++ take_home_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import datetime
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,7 +43,8 @@ INSTALLED_APPS = [
     'photos',
     'corsheaders',
     'django_filters',
-    'rest_framework_bulk'
+    'rest_framework_bulk',
+    'django_heroku'
 ]
 
 MIDDLEWARE = [
@@ -158,4 +160,4 @@ JWT_AUTH = {
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-
+django_heroku.settings(locals())
