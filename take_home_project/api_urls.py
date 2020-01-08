a4f755ef4f27987b9e5a01961a166efae69a8648
Eric H Martanto <1513209601@qq.com>
2020-01-09 01:28:57 +0700

    take_home_project django project

---
+++ take_home_project/api_urls.py
@@ -0,0 +1,13 @@
+# from django.urls import path
+# from rest_framework.routers import SimpleRouter
+#
+# from photos.views import ListPhoto,UpdatePhoto
+#
+# app_name = 'api_urls'
+# router = SimpleRouter()
+#
+# urlpatterns = [
+#
+#     path('photos/', ListPhoto.as_view()),
+#     path('photos/update', UpdatePhoto.as_view())
+# ]
